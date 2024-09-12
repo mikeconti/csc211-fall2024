ce82fa06c23d56c29221f184f7547c732cc07904
Michael Conti <michaelconti@uri.edu>
2024-09-12 19:40:57 -0400

    lec code

---
+++ code/addition.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main(){
+
+    int a,b;
+
+    // std::cout << "Enter a value for A" << std::endl;
+    // std::cout << "Enter a value for B";
+
+    std::cin >> a;
+    std::cin >> b;
+
+    int c = a + b;
+
+    std::cout << a << " + " << b << " = " << c; 
+    
+
+
+    return 0;
+}
